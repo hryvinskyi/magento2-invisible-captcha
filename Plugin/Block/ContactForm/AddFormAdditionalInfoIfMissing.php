be1dab2357ca548ebc674528847ca455c2f0dcde
Vladymyr Hrivinskyi <volodymyr@hryvinskyi.com>
2020-08-13 22:19:25 +0300

    Update AddFormAdditionalInfoIfMissing.php

--- Plugin/Block/ContactForm/AddFormAdditionalInfoIfMissing.php
+++ Plugin/Block/ContactForm/AddFormAdditionalInfoIfMissing.php
@@ -9,14 +9,40 @@ declare(strict_types=1);
 
 namespace Hryvinskyi\InvisibleCaptcha\Plugin\Block\ContactForm;
 
+use Hryvinskyi\InvisibleCaptcha\Model\Verify\Contact;
 use Magento\Framework\Pricing\Render;
 use Magento\Contact\Block\ContactForm as Subject;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class AddFormAdditionalInfoIfMissing
  */
 class AddFormAdditionalInfoIfMissing
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var Contact
+     */
+    private $verifyContact;
+
+    /**
+     * AddFormAdditionalInfoIfMissing constructor.
+     *
+     * @param Contact $verifyContact
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        Contact $verifyContact,
+        LoggerInterface $logger
+    ) {
+        $this->verifyContact = $verifyContact;
+        $this->logger = $logger;
+    }
+
     public function beforeToHtml(
         Subject $subject
     ) {
